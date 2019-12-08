204ac5371dda95d2544b4ee7e0919c2888b7883c
Nawi Kartini <nawikartini@gmail.com>
2019-12-09 02:47:43 +0800

    build server

--- db/psql/psql.go
+++ db/psql/psql.go
@@ -1,28 +1,29 @@
 package psql
 
 import (
-	_ "github.com/lib/pq"
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "root"
-	dbname   = "agoda"
-  )
+	host     = "db-postgresql-sgp1-agoda-do-user-4803185-0.db.ondigitalocean.com"
+	port     = 25060
+	user     = "doadmin"
+	password = "ea4xruzd6cjyhfe5"
+	dbname   = "defaultdb"
+)
 
 func Connect() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		fmt.Println(err)
-        return nil, err
-    }
-    return db, nil
+		return nil, err
+	}
+	return db, nil
 }
